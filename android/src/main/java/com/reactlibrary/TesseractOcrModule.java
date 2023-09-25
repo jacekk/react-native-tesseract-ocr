0589eeb29e6fda93b51816ef01271744dbf0207c
jacekk <jacek.kapias@amsterdamstandard.com>
2023-09-25 12:49:54 +0200

    fix: possible fix for "Could not create directory" error;

--- android/src/main/java/com/reactlibrary/TesseractOcrModule.java
+++ android/src/main/java/com/reactlibrary/TesseractOcrModule.java
@@ -41,6 +41,7 @@ public class TesseractOcrModule extends ReactContextBaseJavaModule {
     public TesseractOcrModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        this.DATA_PATH = reactContext.getExternalFilesDir(null).toString();
         if (!this.DATA_PATH.contains(reactContext.getPackageName())) {
             this.DATA_PATH += File.separator + reactContext.getPackageName();
         }
